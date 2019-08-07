c029774f1d63542e118055426f5b66fc08696932
Alexandre Almeida da Costa <53007136+alxcost@users.noreply.github.com>
2019-08-07 18:19:18 +0100

    This should work;

--- R/repClonality.R
+++ R/repClonality.R
@@ -4,17 +4,12 @@ repClonality <- function (dataframe) {
   # check if the input vector is valid (i.e. meets DataSHIELD privacy criteria)
   check <- isValidDS(dataframe)
   
-  print(check)
-  
-  library(immunarch)
-  ret <- parse_mixcr(dataframe)
-  
   # return missing value if the input vector is not valid
-  if(!check) {
-    result <- repClonality(ret, .gene = "clonal.prop")
+  if (!check) {
+    library(immunarch)
+    ret <- parse_mixcr(dataframe)
+    return (immunarch::repClonality(ret, .gene = "clonal.prop"))
   } else {
-    result <- NA
+    return (NA)
   }
-  
-  return(result)
 }
