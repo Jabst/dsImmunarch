0e0f95a2e5a74a165ada33ba8dfbb0f567d4dcbb
Alexandre Almeida da Costa <53007136+alxcost@users.noreply.github.com>
2019-08-07 18:14:37 +0100

    Again;

--- R/repClonality.R
+++ R/repClonality.R
@@ -3,11 +3,34 @@ repClonality <- function (dataframe) {
   
   print(dim(dataframe))
   
-  print(class(dataframe) == "data.frame")
-  print(dim(dataframe)[1])
-  nfilter <- dsBase::setFilterDS()
-  print(nfilter)
-  
+  if(class(dataframe) == "character" | class(dataframe) == "integer" | class(dataframe) == "logical" | class(dataframe) == "numeric") {
+    if(length(dataframe) > 0 & length(dataframe)  < 5) {
+      
+      print(1)
+    } else {
+      print(2)
+    }
+  }else{
+    if(class(dataframe) == "factor"){
+      tt <- tabulate(dataframe)
+      xx <- which(tt > 0 & tt < 5)
+      if(length(xx) > 0) {
+        print(3)
+      } else {
+        print(4)
+      }
+    }else{
+      if(class(dataframe) == "data.frame" | class(dataframe) == "matrix"){
+        if(dim(dataframe)[1] > 0 & dim(dataframe)[1] < 5){
+          print(5)
+        }else{
+          print(6)
+        }
+      }else{
+        print(7)
+      }
+    }
+  } 
   
   # check if the input vector is valid (i.e. meets DataSHIELD privacy criteria)
   check <- isValidDS(dataframe)
