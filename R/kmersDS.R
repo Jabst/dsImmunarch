7025355421063ee65702e7e95049563cdefb9f9c
Alexandre Almeida da Costa <53007136+alxcost@users.noreply.github.com>
2019-08-08 17:45:58 +0100

    Fixed kmers argument;

--- R/kmersDS.R
+++ R/kmersDS.R
@@ -2,25 +2,16 @@ kmersDS <- function (dataframe) {
   # check if the input vector is valid (i.e. meets DataSHIELD privacy criteria)
   check <- isValidDS(dataframe)
   
-  print(1)
-  
   # return missing value if the input vector is not valid
   if (!check) {
     return (NA)
   }
   
-  print(2)
-  
   library(immunarch)
   ret <- parse_mixcr(dataframe)
   
-  print(3)
-  
-  kmers = getKmers(dataframe, 10, .col = "aa")
-  print(4)
+  kmers = getKmers(ret, 10, .col = "aa")
   kmersProfie = kmer_profile(kmers, "prob")
-  
-  print(5)
-  
+
   return (kmersProfie)
 }
