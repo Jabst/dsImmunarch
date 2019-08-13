9420b98617d560b344ae36495659c38cddb6e37b
Alexandre Almeida da Costa <53007136+alxcost@users.noreply.github.com>
2019-08-13 18:04:29 +0100

    Added missing library, although I'm sure why it would need this...?

--- R/geneUsageDS.R
+++ R/geneUsageDS.R
@@ -1,4 +1,6 @@
 geneUsageDS <- function (dataframe, gene) {
+  library(dsBase)
+  
   # check if the input vector is valid (i.e. meets DataSHIELD privacy criteria)
   if(!isValidDS(dataframe)) {
     return (NA)
